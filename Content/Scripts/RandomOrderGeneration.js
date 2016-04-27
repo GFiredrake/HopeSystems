f5279a52ed4c89202f7fa7affb861526dbc4f832
Robin Windon <robin.windon@hotmail.com>
2016-04-27 17:22:04 +0100

    Random generator commit 2

--- Content/Scripts/RandomOrderGeneration.js
+++ Content/Scripts/RandomOrderGeneration.js
@@ -1,5 +1,5 @@
 ﻿function GenerateRandomOrder() {
-
+    $('#GenerateButton').addClass('HiddenDiv')
     var rates = document.getElementById('Condition1').checked;
 
     var condition = 0
@@ -21,9 +21,52 @@
     })
     .done(function (data) {
         var pause = 1;
+
+        $('#DisplayTable').append("<tr>" +
+                                          "<td>Order Id</td>" +
+                                          "<td>Customer Id</td>" +
+                                          "<td>Title</td>" +
+                                          "<td>First Name</td>" +
+                                          "<td>Last Name</td>" +
+                                          "<td>Email Address</td>" +
+                                          "<td>Phone Number</td>" +
+                                      "</tr>");
+
+        GenerateAdditionalLinesSales(data)
+
         
     })
     .fail(function (xhr, status, error) {
         alert("an error has occured, Please try again. if this problem persists please seak technical support : " + xhr.responseText);
     });
+}
+
+$("#Condition1").change(function () {
+    $("#StartDateDiv").removeClass("HiddenDiv")
+    $("#EndDateDiv").removeClass("HiddenDiv")
+    $("#SkuDiv").addClass("HiddenDiv")
+});
+
+$("#Condition2").change(function () {
+    $("#StartDateDiv").removeClass("HiddenDiv")
+    $("#EndDateDiv").removeClass("HiddenDiv")
+    $("#SkuDiv").removeClass("HiddenDiv")
+});
+
+$("#Condition3").change(function () {
+    $("#StartDateDiv").removeClass("HiddenDiv")
+    $("#EndDateDiv").addClass("HiddenDiv")
+    $("#SkuDiv").addClass("HiddenDiv")
+});
+
+function GenerateAdditionalLinesSales(item, tableNumber) {
+    var content = "<tr>"
+    for (key in item) {
+            var value = item[key];
+            if(value != null )
+            content += '<td>' + value + '</td>';
+    }
+
+    content += "</tr>"
+    $('#DisplayTable').append(content);
 }
