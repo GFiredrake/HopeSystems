4bd45a6c80176c1f1079e21392e32b8b8d8570cc
Robin Windon <robin.windon@hotmail.com>
2016-05-04 15:22:00 +0100

    sku edit 1

--- Controllers/TvController.cs
+++ Controllers/TvController.cs
@@ -57,6 +57,36 @@ namespace InternalWebSystems.Controllers
             return View();
         }
 
+        //Sku Edit
+        public ActionResult SkuEdit()
+        {
+            //Repetitition need to find a way to extract
+            #region Repeated Page validation and navigation controll
+            bool MyCookie = IsCookiePresentAndSessionValid("HIWSSettings");
+            ViewBag.TodaysDate = DateTime.Today.ToString("yyyy-MM-dd");
+            if (MyCookie == false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            HttpCookie aCookie = Request.Cookies["HIWSSettings"];
+            List<string> somelist = new LogOnController().GenerateMenuForSession(aCookie["SessionGui"]);
+            if (somelist != null)
+            {
+                StringBuilder astring = new StringBuilder();
+                foreach (string buttonString in somelist)
+                {
+                    astring.Append(buttonString);
+                }
+
+                ViewBag.MenuHtml = astring.ToString();
+            }
+            #endregion
+            //End Repetition 
+
+            return View();
+        }
+
         //Minuets
         public ActionResult Minutes()
         {
@@ -694,5 +724,6 @@ namespace InternalWebSystems.Controllers
 
             return returnedList;
         }
+
     }
 }
